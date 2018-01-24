7e44e2147a9adca02caa091f34a5a73dfe638801
Huang Li <huangliop@163.com>
2018-01-24 14:39:34 +0800

    修复设置的值为0时，持久化会失败的问题

--- src/stores/PersistData.js
+++ src/stores/PersistData.js
@@ -13,7 +13,7 @@ class PersistData {
     reaction(
       () => store[name],
       data => {
-        if (data) {
+        if (typeof data === 'undefined') {
           const type = typeof data;
           data =
             type === 'string' || type === 'number'
